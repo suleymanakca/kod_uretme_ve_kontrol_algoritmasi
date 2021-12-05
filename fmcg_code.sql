40808d2d28b46bad66db2e24106239146c67a416
Süleyman Akça <sakca3253@gmail.com>
2021-12-05 15:18:08 +0300

    first commit

---
+++ fmcg_code.sql
@@ -0,0 +1,16 @@
+USE [kaizen_case_Q1]
+GO
+
+/****** Object:  Table [dbo].[tbl_fmcg_code]    Script Date: 5.12.2021 15:12:53 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[tbl_fmcg_code](
+	[a_kod] [nvarchar](10) NULL
+) ON [PRIMARY]
+GO
+
+
